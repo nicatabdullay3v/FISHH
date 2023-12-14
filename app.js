e66a78d1409d5f489aa883841084c8326ae8a41a
nicatabdullay3v <nicat.neon@bk.ru>
2023-12-14 19:46:24 +0400

    OOP

--- app.js
+++ app.js
@@ -16,38 +16,174 @@ var __extends = (this && this.__extends) || (function () {
         d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
     };
 })();
-var LivePlace;
-(function (LivePlace) {
-    LivePlace[LivePlace["river"] = 0] = "river";
-    LivePlace[LivePlace["ocean"] = 1] = "ocean";
-    LivePlace[LivePlace["sea"] = 2] = "sea";
-})(LivePlace || (LivePlace = {}));
-var Animal = /** @class */ (function () {
-    function Animal(name, age) {
-        this.name = name;
+var Human = /** @class */ (function () {
+    function Human(name, surname, age) {
+        this._name = name;
+        this._surname = surname;
         this.age = age;
     }
-    Animal.prototype.getInfo = function () {
-        console.log(this.name, this.age);
-    };
-    return Animal;
+    Object.defineProperty(Human.prototype, "name", {
+        get: function () {
+            return this._name;
+        },
+        enumerable: false,
+        configurable: true
+    });
+    Object.defineProperty(Human.prototype, "surname", {
+        get: function () {
+            return this._surname;
+        },
+        enumerable: false,
+        configurable: true
+    });
+    Object.defineProperty(Human.prototype, "surnameAndName", {
+        get: function () {
+            return this._surname + this._name;
+        },
+        enumerable: false,
+        configurable: true
+    });
+    return Human;
 }());
-var Fish = /** @class */ (function (_super) {
-    __extends(Fish, _super);
-    function Fish(name, age, livePlace, kill) {
-        var _this = _super.call(this, name, age) || this;
-        _this.livePlace = livePlace;
-        _this.kill = kill;
+// EMPLYOOOOOOOOOOOOOOOOOOOOOOO
+var Position;
+(function (Position) {
+    Position["boss"] = "boss";
+    Position["other"] = "other";
+})(Position || (Position = {}));
+var Employee = /** @class */ (function (_super) {
+    __extends(Employee, _super);
+    function Employee(name, surname, age, salary, skills, position) {
+        var _this = _super.call(this, name, surname, age) || this;
+        // this.employeeName = name;
+        // this.employeeSurname = surname;
+        _this.salary = salary;
+        _this.skills = skills;
+        _this.position = position;
+        if (typeof Number(age)) {
+            _this.age = 0;
+        }
+        else {
+            _this.age = age;
+        }
         return _this;
     }
-    Fish.prototype.swim = function () {
-        console.log("uzur baliq");
+    Employee.prototype.getInfo = function () {
+        return " Age:".concat(this.age, " Salary: ").concat(this.salary, " Surname: ").concat(this.surname, " Name:").concat(this.name);
     };
-    Fish.prototype.getInfo = function () {
-        console.log("Fish: ".concat(this.name, ", Age: ").concat(this.age, ",Color: ").concat(LivePlace[this.livePlace], ",kill: ").concat(this.kill));
+    return Employee;
+}(Human));
+// Studentttttttttttttttttttttttttttttt
+var Employee1 = new Employee("salam", "salam", 23, 233, ["saa"], Position.boss);
+Employee1.getInfo();
+console.log(Employee1);
+var Student = /** @class */ (function (_super) {
+    __extends(Student, _super);
+    function Student(name, surname, age, groupName, hobbies, GPA) {
+        var _this = _super.call(this, name, surname, age) || this;
+        _this.groupName = groupName;
+        _this.hobbies = hobbies;
+        _this._GPA = GPA;
+        return _this;
+    }
+    Student.prototype.hasPassed = function () {
+        if (this.GPA < 50) {
+            return true;
+        }
+        else {
+            return false;
+        }
     };
-    return Fish;
-}(Animal));
-var myFish = new Fish("Nemo", 2, LivePlace.ocean, "nese");
-myFish.getInfo();
-myFish.swim();
+    Student.prototype.getInfo = function () {
+        console.log("Employee: ".concat(this.name, " ").concat(this.surname, ", Age: ").concat(this.age));
+        return "salam";
+    };
+    Object.defineProperty(Student.prototype, "GPA", {
+        get: function () {
+            return this._GPA;
+        },
+        set: function (NewGPA) {
+            if (NewGPA <= 100 && NewGPA > 0) {
+                this._GPA = NewGPA;
+            }
+        },
+        enumerable: false,
+        configurable: true
+    });
+    return Student;
+}(Human));
+var studentForm = document.querySelector(".student");
+var employeeForm = document.querySelector(".employee");
+var selectOption = document.querySelector(".select");
+function selectreset() {
+    selectOption.addEventListener("change", function () {
+        if (this.value == "employee") {
+            studentForm.style.display = "none";
+            employeeForm.style.display = "block";
+        }
+        else if (this.value == "student") {
+            studentForm.style.display = "block";
+            employeeForm.style.display = "none";
+        }
+    });
+}
+selectreset();
+var employeeName = document.querySelector(".employeeName");
+var employeeSurname = document.querySelector(".employeeSurname");
+var employeeAge = document.querySelector(".employeeAge");
+var employeeSalary = document.querySelector(".employeeSalary");
+var employeeSkills = document.querySelector(".employeeSkills");
+var positions = document.querySelector(".skills");
+var position;
+positions.addEventListener("change", function () {
+    position = positions.value;
+});
+console.log(employeeSalary);
+function reset() {
+    employeeAge.value = "";
+    employeeName.value = "";
+    employeeSurname.value = "";
+    employeeSalary.value = "";
+    employeeSkills.value = "";
+    position = "";
+}
+employeeForm.addEventListener("submit", function (e) {
+    e.preventDefault();
+    var age = +(employeeAge.value);
+    var Name = employeeName.value;
+    var Surname = employeeSurname.value;
+    var salary = +(employeeSalary.value);
+    var skills = (employeeSkills.value).split(",");
+    var postionn = position;
+    var employee5 = new Employee(Name, Surname, age, salary, skills, Position.boss);
+    console.log(employee5);
+    var Employes = document.querySelector(".Employes");
+    Employes.innerHTML += " <li>".concat(employee5.getInfo(), "</li>\n\n");
+    reset();
+    if (selectOption.value == "employee") {
+        studentForm.style.display = "block";
+        employeeForm.style.display = "none";
+        selectOption.value = "student";
+    }
+});
+studentForm.addEventListener("submit", function (e) {
+    e.preventDefault();
+    var age = +(employeeAge.value);
+    var Name = employeeName.value;
+    var Surname = employeeSurname.value;
+    var salary = +(employeeSalary.value);
+    var skills = (employeeSkills.value).split(",");
+    var postionn = position;
+    var employee5 = new Employee(Name, Surname, age, salary, skills, Position.boss);
+    console.log(employee5);
+    var Employes = document.querySelector(".Employes");
+    Employes.innerHTML += " <li>".concat(employee5.getInfo(), "</li>\n\n");
+    reset();
+    if (selectOption.value == "employee") {
+        studentForm.style.display = "block";
+        employeeForm.style.display = "none";
+        selectOption.value = "student";
+    }
+});
+var salam = "sadsa";
+console.log(typeof salam);
--- app.ts
+++ app.ts
@@ -182,62 +182,270 @@
 // console.log();
 // const myCar = new Car(VehicleType.car, 'Toyota', 'Camry', new Date(2022, 0, 1), 60, 12, engineType.doch, 30, 50000);
 // console.log(myCar.getInfo());
-interface IAnimal {
-  name: string;
-  age: number;
-  getInfo(): void;
+// interface IAnimal {
+//   name: string;
+//   age: number;
+//   getInfo(): void;
+// }
+// enum LivePlace {
+//   river,
+//   ocean,
+//   sea
+// }
+// type killType = string | string[]
+// interface IKill<T extends killType> {
+//   kill: T
+// }
+// interface IFish extends IKill<killType> {
+//   swim(): void,
+//   livePlace: LivePlace
+//   kill: killType
+
+// }
+
+// abstract class Animal implements IAnimal {
+//   constructor(
+//     readonly name: string,
+//     readonly age: number
+//   ) { }
+
+//   getInfo(): void {
+//     console.log(this.name, this.age);
+//   }
+// }
+
+// class Fish extends Animal implements IFish {
+//   livePlace: LivePlace
+//   kill: killType;
+//   constructor(
+//     name: string,
+//     age: number,
+//     livePlace: LivePlace,
+//     kill: killType,
+
+//   ) {
+//     super(name, age);
+//     this.livePlace = livePlace;
+//     this.kill = kill
+//   }
+//   swim(): void {
+//     console.log("uzur baliq");
+
+//   }
+//   getInfo(): void {
+//     console.log(`Fish: ${this.name}, Age: ${this.age},Color: ${LivePlace[this.livePlace]},kill: ${this.kill}`);
+//   }
+// }
+
+
+// const myFish = new Fish("Nemo", 2, LivePlace.ocean, "2343556");
+// myFish.getInfo();
+// myFish.swim() 
+
+interface IHuman {
+
+    name: string;
+    surname: string;
+    age: number;
+    getInfo(): string;
 }
-enum LivePlace {
-  river,
-  ocean,
-  sea
+abstract class Human implements IHuman {
+    private _name: string;
+    private _surname: string;
+    public age: number
+    constructor(name: string, surname: string, age: number) {
+        this._name = name;
+        this._surname = surname;
+        this.age = age;
+    }
+
+    public get name() {
+        return this._name
+    }
+    public get surname() {
+        return this._surname
+    }
+    public get surnameAndName() {
+        return this._surname + this._name
+    }
+
+    abstract getInfo(): string;
 }
-type killType = string | string[]
-interface IKill<T extends killType> {
-  kill: T
+// EMPLYOOOOOOOOOOOOOOOOOOOOOOO
+enum Position {
+    boss = "boss",
+    other = "other"
 }
-interface IFish extends IKill<killType> {
-  swim(): void,
-  livePlace: LivePlace
-  kill: killType
+interface IEmployee<T> {
+    salary: number,
+    skills: T,
+    position: Position
 
 }
+class Employee extends Human implements IEmployee<string[]> {
+    // private employeeName: string;
+    // private employeeSurname: string;
+    salary: number;
+    skills: string[];
+    position: Position;
 
-abstract class Animal implements IAnimal {
-  constructor(
-    readonly name: string,
-    readonly age: number
-  ) { }
+    constructor(name: string, surname: string, age: number, salary: number, skills: string[], position: Position,) {
+        super(name, surname, age);
+        // this.employeeName = name;
+        // this.employeeSurname = surname;
+        this.salary = salary
+        this.skills = skills
+        this.position = position
+        if ( typeof Number(age)) {
+           this.age = 0
+            
+        }
+        else{
+            this.age = age
+        }
+    }
 
-  getInfo(): void {
-    console.log(this.name, this.age);
+
+    getInfo(): string {
+
+        return ` Age:${this.age } Salary: ${this.salary } Surname: ${this.surname } Name:${this.name }`
+    }
+}
+// Studentttttttttttttttttttttttttttttt
+let Employee1 = new Employee("salam", "salam", 23, 233, ["saa"], Position.boss)
+
+Employee1.getInfo()
+console.log(Employee1);
+
+interface IStudent<T> {
+    groupName: string,
+    hobbies: T,
+    GPA: number,
+    hasPassed(): boolean
+}
+class Student extends Human implements IStudent<string | string[]> {
+    groupName: string
+    hobbies: string | string[]
+    private _GPA: number
+    constructor(name: string, surname: string, age: number, groupName: string, hobbies: string | string[], GPA: number) {
+        super(name, surname, age);
+        this.groupName = groupName
+        this.hobbies = hobbies
+        this._GPA = GPA
+
+    }
+    hasPassed(): boolean {
+        if (this.GPA < 50) {
+            return true
+        }
+        else {
+            return false
+        }
+    }
+    getInfo(): string {
+        console.log(`Employee: ${this.name} ${this.surname}, Age: ${this.age}`);
+        return "salam"
+    }
+    public get GPA() {
+        return this._GPA
+    }
+    public set GPA(NewGPA) {
+        if (NewGPA <= 100 && NewGPA > 0) {
+            this._GPA = NewGPA
+        }
+
+    }
+}
+
+
+let studentForm = document.querySelector(".student") as HTMLFormElement
+let employeeForm = document.querySelector(".employee") as HTMLFormElement
+let selectOption = document.querySelector(".select") as HTMLSelectElement
+function selectreset(){
+    selectOption.addEventListener("change", function () {
+
+        if (this.value == "employee") {
+            studentForm.style.display = "none"
+            employeeForm.style.display = "block"
+        }
+        else if (this.value == "student") {
+            studentForm.style.display = "block"
+            employeeForm.style.display = "none"
+    
+    
+        }
+    
+    })
+}
+selectreset()
+let employeeName = document.querySelector(".employeeName") as HTMLInputElement
+let employeeSurname = document.querySelector(".employeeSurname") as HTMLInputElement
+let employeeAge = document.querySelector(".employeeAge") as HTMLInputElement
+let employeeSalary = document.querySelector(".employeeSalary") as HTMLInputElement
+let employeeSkills = document.querySelector(".employeeSkills") as HTMLInputElement
+let positions = document.querySelector(".skills") as HTMLSelectElement
+let position: string;
+positions.addEventListener("change", function () {
+    position = positions.value
+})
+console.log(employeeSalary);
+function reset () {
+    employeeAge.value = ""
+    employeeName.value = ""
+    employeeSurname.value = ""
+    employeeSalary.value = ""
+    employeeSkills.value = ""
+    position = ""
+}
+employeeForm.addEventListener("submit", function (e) {
+    e.preventDefault()
+    let age: number = +(employeeAge.value)
+    let Name: string = employeeName.value
+    let Surname: string = employeeSurname.value
+    let salary: number = +(employeeSalary.value)
+    let skills: string[] = (employeeSkills.value).split(",")
+    let postionn: string = position
+
+    let employee5 = new Employee(Name, Surname, age, salary,skills , Position.boss)
+
+    console.log(employee5);
+    let Employes = document.querySelector(".Employes") as HTMLDivElement
+    Employes.innerHTML += ` <li>${employee5.getInfo()}</li>
+
+`
+   reset()
+  if (selectOption.value == "employee") {
+    
+    studentForm.style.display = "block"
+    employeeForm.style.display = "none"
+selectOption.value= "student"
   }
-}
+})
+studentForm.addEventListener("submit", function (e) {
+    e.preventDefault()
+    let age: number = +(employeeAge.value)
+    let Name: string = employeeName.value
+    let Surname: string = employeeSurname.value
+    let salary: number = +(employeeSalary.value)
+    let skills: string[] = (employeeSkills.value).split(",")
+    let postionn: string = position
 
-class Fish extends Animal implements IFish {
-  livePlace: LivePlace
-  kill: killType;
-  constructor(
-    name: string,
-    age: number,
-    livePlace: LivePlace,
-    kill: killType,
+    let employee5 = new Employee(Name, Surname, age, salary,skills , Position.boss)
 
-  ) {
-    super(name, age);
-    this.livePlace = livePlace;
-    this.kill = kill
+    console.log(employee5);
+    let Employes = document.querySelector(".Employes") as HTMLDivElement
+    Employes.innerHTML += ` <li>${employee5.getInfo()}</li>
+
+`
+   reset()
+  if (selectOption.value == "employee") {
+    
+    studentForm.style.display = "block"
+    employeeForm.style.display = "none"
+selectOption.value= "student"
   }
-  swim(): void {
-    console.log("uzur baliq");
-
-  }
-  getInfo(): void {
-    console.log(`Fish: ${this.name}, Age: ${this.age},Color: ${LivePlace[this.livePlace]},kill: ${this.kill}`);
-  }
-}
+})
 
 
-const myFish = new Fish("Nemo", 2, LivePlace.ocean, "nese");
-myFish.getInfo();
-myFish.swim()
+let salam = "sadsa"
+console.log(typeof salam);
